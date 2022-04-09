20d2cbd2bf4f688dd040ce8b53a691fa598bc37f
JuanMunoz17 <103296638+JuanMunoz17@users.noreply.github.com>
2022-04-09 00:48:26 -0500

    Avance Creacion Card
    Se agrego imagen de sensor, estructura de la card y ya

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{...styles.basic, ...styles.card}}>
+      <View style={styles.basic}>
+        <Image
+          style={{ width: 48, height: 48 }}
+          source={require("./src/assets/tempSensor.svg")}
+        />
+      </View>
+      <Text style={styles.text}>Sensor de Temperatura</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  basic: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: "center",
+    justifyContent: "center",
+  }, 
+  text: {
+    color: "#117CDF",
+  }, 
+  card:{
+    width: 105,
+    height: 124,
+    backgroundColor: "red",
+  }
 });
