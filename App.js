671ba53314616e59180bafc2f8ed490781ab9dd7
YosstinCode <yosstincode@gmail.com>
2022-04-28 17:22:00 -0500

    Adding react native

--- App.js
+++ App.js
@@ -1,45 +1,26 @@
-import { StyleSheet, View} from "react-native";
-import BtnSensor from "./src/components/BtnSensor";
-import Svgtemp from "./src/assets/svgTemp";
-import SvgMotion from "./src/assets/SvgMotion";
-import SvgSmoke from "./src/assets/SvgSmoke";
+import { NavigationContainer} from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./src/screens/Home";
+import SettingsScreen from "./src/screens/Settings";
 
-export default function App() { 
 
-  const svgTemp = () => <Svgtemp height={3.5} width={6.1} />;
-  const svgSmoke = () => <SvgSmoke height={3.4} width={7.6} />;
-  const svgMotion = () => <SvgMotion height={3.2} width={6.7} />;
 
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.grid}>
-      <View style={styles.items}>
-      <BtnSensor
-        name={"sensor de temperatura"}
-        svg={svgTemp()}
-      />
-      <BtnSensor
-        name={"sensor de humo"}
-        svg={svgSmoke()}
-      />
-      <BtnSensor
-        name={"sensor de movimiento"}
-        svg={svgMotion()}
-      />
-      </View>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 }
-const styles = StyleSheet.create({
-  grid: {
-    display:'flex',
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#f4f4f4",
-    flexDirection:'column',
-    
-  },items:{
-    justifyContent: "center",
-    flexDirection:'row',
-    flexWrap:'wrap'
-  }
-});
+
+export default function App() { 
+  return(
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+
+  )
+}
