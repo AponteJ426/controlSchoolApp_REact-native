e9ef69a07b01418565c74961f9ee0a44f40cb9b6
YosstinCode <yosstincode@gmail.com>
2022-04-29 22:12:33 -0500

    Theme add

---
+++ src/Theme.js
@@ -0,0 +1,37 @@
+const Theme={
+    Background:{
+        back:"#F3F3F3",
+        primary:"117CDF",
+        secondary:"FEFCFD",
+        degrade:{
+            color1:"",
+            color2:"",
+        }
+    },
+    FontSize:{
+        sm:8,
+        md:10,
+        lg:12,
+        xl:14,
+        xxl:16,
+    },
+    FontFamily:{
+        roboto:"Roboto",
+    },
+    FontWeight:{
+        light:300,
+        regular:400,
+        medium:500,
+        bold:700,
+
+    },
+    FontColor:{
+        textTitle:Theme.background.primary,
+        textWhite:Theme.background.secondary,
+        text:"737272",
+    },
+    
+    
+}
+
+export default Theme
