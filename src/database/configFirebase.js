f2fb61f8e81d61b9228a7bb57da462b10c32b1d9
Juan Ap <juncaper0123@gmail.com>
2022-05-10 16:03:32 -0500

    fix/conectionFirebase:backend

---
+++ src/database/configFirebase.js
@@ -0,0 +1,13 @@
+
+ export default  firebaseConfig = {
+  apiKey: "AIzaSyBAfxqtQXFSFOI2QvFijlFRuWnpHSFwJVU",
+  authDomain: "colegio-domotico.firebaseapp.com",
+  databaseURL: "https://colegio-domotico-default-rtdb.firebaseio.com",
+  projectId: "colegio-domotico",
+  storageBucket: "colegio-domotico.appspot.com",
+  messagingSenderId: "218004076593",
+  appId: "1:218004076593:web:0e1c3943c97e4158673d10",
+  measurementId: "G-MMKMR67PM9",
+};
+
+
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,49 +1,86 @@
-import { StyleSheet, View,Button} from "react-native";
 import BtnSensor from "../components/BtnSensor";
 import Svgtemp from "../assets/svgTemp";
 import SvgMotion from "../assets/SvgMotion";
 import SvgSmoke from "../assets/SvgSmoke";
 
+import { initializeApp } from "firebase/app";
+import React, { useState } from "react";
 
-export default function Home() { 
+import { getDatabase, ref, onValue, set } from "firebase/database";
+import { View, StyleSheet } from "react-native";
+
+import firebaseConfig from "../database/configFirebase.js";
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs(["Setting a timer"]);
+
+export default function Home() {
   const svgTemp = () => <Svgtemp height={3.5} width={6.1} />;
+
   const svgSmoke = () => <SvgSmoke height={3.4} width={7.6} />;
+
   const svgMotion = () => <SvgMotion height={3.2} width={6.7} />;
+
+  initializeApp(firebaseConfig);
+
+  const [dataTemp, setdataTemp] = useState({});
+
+  const db = getDatabase();
+
+  const sendData = (name) => {
+    let dataValue = 0;
+    const reference = ref(db, "circuitos/" + name);
+    onValue(reference, (snapshot) => {
+      dataValue = snapshot.val();
+    });
+
+    dataValue == 1 ? true : false;
+    set(reference, dataValue == 1 ? 0 : 1);
+    setdataTemp({ ...dataTemp, [name]: dataValue });
+  };
 
   return (
     <View style={styles.grid}>
       <View style={styles.items}>
-      <BtnSensor
-        name={"sensor de temperatura"}
-        svg={svgTemp()}
+        <BtnSensor
+          name={"sensor de temperatura"}
+          idName={'sensorTemp'}
+          svg={svgTemp()}
+          setFirebase={sendData}
+          activate={dataTemp.sensorTemp}
         />
-      <BtnSensor
-        name={"sensor de humo"}
-        svg={svgSmoke()}
+        <BtnSensor
+          name={"sensor de humo"}
+          svg={svgSmoke()}
+          setFirebase={sendData}
+          activate={dataTemp.sensorHumo}
+          idName={'sensorHumo'}
+
         />
-      <BtnSensor
-        name={"sensor de movimiento"}
-        svg={svgMotion()}
+        <BtnSensor
+          name={"sensor de movimiento"}
+          svg={svgMotion()}
+          setFirebase={sendData}
+          activate={dataTemp.sensorMov}
+          idName={'sensorMov'}
+
         />
       </View>
-
-     
     </View>
   );
 }
 const styles = StyleSheet.create({
   grid: {
-    display:'flex',
+    display: "flex",
     flex: 1,
     justifyContent: "center",
     backgroundColor: "#f4f4f4",
-    flexDirection:'column',
-    
-    
-  },items:{
+    flexDirection: "column",
+  },
+  items: {
     justifyContent: "center",
-    flexDirection:'row',
-    flexWrap:'wrap',
-    marginBottom:150
-  }
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginBottom: 150,
+  },
 });
